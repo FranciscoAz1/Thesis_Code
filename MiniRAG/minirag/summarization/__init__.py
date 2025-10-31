0f3b3b7241c42ec99aaa89de33175ec7ba5ab0ec
FranciscoAz1 <azeredo2000@gmail.com>
2025-10-31 17:28:05 +0000

    Remove subdirectories from tracking to prepare for submodules

--- MiniRAG/minirag/summarization/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Summarization module for MiniRAG."""
-
-from minirag.summarization.bart_summarizer import (
-    BARTSummarizer,
-    TRANSFORMERS_AVAILABLE,
-)
-
-__all__ = ["BARTSummarizer", "TRANSFORMERS_AVAILABLE"]
